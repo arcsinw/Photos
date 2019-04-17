13136bcf7b2138a13e10ae6ffbc4be3715d2ab3c
arcsinw <wangx86@live.com>
2019-04-17 22:23:40 +0800

    Initial commit with nothing worked

---
+++ app/src/main/java/com/arcsinw/photos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.arcsinw.photos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
